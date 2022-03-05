5c3efc1a173696e640c21f647696af51e2a36bcb
Santhoshkumard11 <santhoshkdhana@gmail.com>
2022-03-05 16:18:37 +0530

    Working - image upload to blob storage and detection by the model

--- detect_type/__init__.py
+++ detect_type/__init__.py
@@ -1,12 +1,14 @@
 import logging
 import azure.functions as func
-
+from helpers.predictor import detect_type
+from detect_type.utils import process_image
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("Skin Cancer detection endpoint called")
-    
+    logging.info("Skin Cancer detection endpoint called..")
+
     # set the image name to default for testing
     test_image_path = "images/test.png"
+    prediction = ""
     if req.method == "GET":
             
         image_name = req.params.get('image_name')
@@ -14,8 +16,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             return func.HttpResponse("""Thanks for checking out the api. It works!!
                                      You can pass image_name with test.png as params to get a sample detection of the model.""", status_code=200)
         else:
+
+            prediction_result = detect_type(test_image_path)
+            
             return func.HttpResponse(
-                f"Image name - {image_name} dummy result",
+                f"The prediction has been made - it's {prediction_result}",
                 status_code=200
             )
 
@@ -23,11 +28,17 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         # get the binary image from the request body
         req_body = req.get_json()
         image_binary = req_body.get('image_binary')["$content"]
+        user_id = req_body.get("user_id", "")
+        user_name = req_body.get("user_name", "")
 
         try:
-            # call the detection model method
-            # test_result = detect(image_binary)
-            logging.info(f"Binary image data {str(image_binary)[:100]}")
+            status, prediction = process_image(image_binary, user_id, user_name)
+
+            if not status or prediction is "":
+                return func.HttpResponse(
+                "Something went wrong during processing. Check the Application logs for more info",
+                status_code=200
+            )
 
         except Exception as e:
             logging.exception(f"Here is the error: \n{e}")
@@ -36,7 +47,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Skin Cancer detection ended")
     
     return func.HttpResponse(
-                "dummy result from post method",
+                prediction,
                 status_code=200
             )
         
